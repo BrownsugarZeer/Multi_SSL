bcf31dca842813768d4f6fdfaa0771a069d444b6
BrownsugarZeer <t109318048wu@gmail.com>
2025-01-22 23:15:38 +0800

    refactor(ssl): simplify microphone initialization and update doa_detection function signature

--- multi_ssl/utils/__init__.py
+++ multi_ssl/utils/__init__.py
@@ -1,4 +1,12 @@
-from .bss import Duet
-from .cmd_parser import parsing_params
-from .find_peaks import find_peak_indices
-from .ssl import inti_mics, doa_detection
+from multi_ssl.utils.bss import Duet
+from multi_ssl.utils.cmd_parser import parsing_params
+from multi_ssl.utils.find_peaks import find_peak_indices
+from multi_ssl.utils.ssl import doa_detection
+
+
+__all__ = [
+    "Duet",
+    "parsing_params",
+    "find_peak_indices",
+    "doa_detection",
+]
--- multi_ssl/utils/ssl.py
+++ multi_ssl/utils/ssl.py
@@ -1,51 +1,23 @@
-import numpy as np
 import torch
 from speechbrain.processing.features import STFT
 from speechbrain.processing.multi_mic import Covariance, SrpPhat
 
 
-def inti_mics(rotation=0):
-    """
-    Initialize the microphone array coordinates in Euclidean coordinates.
-
-    Augments
-    --------
-    rotation : int
-        Use a rotation matrix to rotate the microphone array
-        coordinates counterclockwise.
-
-    Returns
-    -------
-    mics : Tensor
-        Return the coordinates of (x, y, z).
-    """
-
-    mics = torch.zeros((4, 3))
-    mics[0, :] = torch.Tensor([-0.02285, -0.02285, +0.005])
-    mics[1, :] = torch.Tensor([+0.02285, -0.02285, +0.005])
-    mics[2, :] = torch.Tensor([+0.02285, +0.02285, +0.005])
-    mics[3, :] = torch.Tensor([-0.02285, +0.02285, +0.005])
-
-    if rotation:
-        sin = np.sin(rotation * np.pi / 180.0)
-        cos = np.cos(rotation * np.pi / 180.0)
-        r_mat = torch.Tensor([[cos, sin, 0.0], [-sin, cos, 0.0], [0.0, 0.0, 1.0]])
-        mics = torch.matmul(mics, r_mat)
-    return mics
-
-
-def doa_detection(waveform, mics=None):
+def doa_detection(mics, waveform, sample_rate=16000):
     """
     Using the SRP-PHAT to determine the direction of angles.
     Augments
     --------
+    mics : torch.Tensor
+        The cartesian coordinates (xyz) in meters of each microphone.
+        The tensor must have the following format (n_mics, 3).
     waveform : torch.Tensor
         the input shape is [batch, time_step, channel], and
         the number of channels should be at least 4.
+    sample_rate : int
+        the sample rate of the input waveform.
     """
-    if mics is None:
-        mics = inti_mics()
-    stft = STFT(sample_rate=16000)
+    stft = STFT(sample_rate=sample_rate)
     cov = Covariance()
     srpphat = SrpPhat(mics=mics)
 
