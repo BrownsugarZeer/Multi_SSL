97e28dc66d9ac6ab346f9d815a49730677c4d2cc
BrownsugarZeer <t109318048wu@gmail.com>
2025-01-22 23:14:22 +0800

    feat(audio): enhance microphone chunk processing with VAD model integration

--- multi_ssl/mic/audio_recorder.py
+++ multi_ssl/mic/audio_recorder.py
@@ -9,34 +9,38 @@ Original code repository:
 """
 
 import numpy as np
+import torch
+from typing import Generator, Tuple
 from collections import deque
 
-from .microphone_stream import MicrophoneStream
-from .simple_vad import VoiceActivityDetection
+from multi_ssl.mic.microphone_stream import MicrophoneStream
+
+torch.set_num_threads(1)
 
 
 def get_microphone_chunks(
-    min_to_cumulate=5,  # 0.5 seconds
-    max_to_cumulate=20,  # 2.0 seconds
+    microphone_stream: MicrophoneStream,
+    *,
+    min_to_cumulate=5,
+    max_to_cumulate=20,
     precumulate=5,
-    max_to_visualize=20,
-    resample_rate=None,
-    **kwargs,
-):
+    speech_threshold=0.8,
+) -> Generator[Tuple[int, np.ndarray], None, None]:
     """
     Real-time VAD(voice-activity-detection) chunk catcher.
 
     Augments
     --------
+    microphone_stream : MicrophoneStream
+        the microphone stream object.
     min_to_cumulate : int
-        the minimum length of waveform, default is 0.5 seconds.
+        the minimum length of waveform, default is 5 chunks.
     max_to_cumulate : int
-        the maximun length of waveform, default is 2.0 seconds.
+        the maximun length of waveform, default is 20 chunks.
     precumulate : int
-    max_to_visualize : int
-        the initial length of waveform, default is 2.0 seconds.
-    resample_rate : int:
-        if resample_rate is None, that means the resample_rate is 16000 as default.
+        the length of waveform to keep before the speech, default is 5 chunks.
+    speech_threshold : float
+        the threshold to decide if the chunk contains speech, default is 0.8.
 
     Returns
     -------
@@ -45,38 +49,58 @@ def get_microphone_chunks(
     Example
     -------
     >>> import soundfile as sf
-    >>> from speech_assistance.utils.audio_recorder import get_microphone_chunks
-    >>> for sample_rate, waveform in get_microphone_chunks(
-    >>>     rate=16000, chunk=1600, n_channels=6, ignored_channels=[0, 5]
-    >>> ):
-    >>>     # print(waveform)
-    >>>     # print(sample_rate)
-    >>>     sf.write('output.wav', waveform, 16000, 'PCM_16')
+    >>> import numpy as np
+    >>> from multi_ssl.mic.microphone_stream import MicrophoneStream
+    >>> from multi_ssl.mic.audio_recorder import get_microphone_chunks
+    >>> waveforms = []
+    >>> microphone_stream = MicrophoneStream(
+    >>>     rate=16000,
+    >>>     chunk=1600,
+    >>>     channels=6,
+    >>>     ignored_channels=[0, 5],
+    >>> )
+    >>> try:
+    >>>     for sample_rate, waveform in get_microphone_chunks(
+    >>>         microphone_stream,
+    >>>         precumulate=0,
+    >>>     ):
+    >>>         print(sample_rate, waveform.shape)  # 16000, (1600, 4)
+    >>>         waveforms.append(waveform)
+    >>> finally:
+    >>>     sf.write("output.wav", np.concatenate(waveforms, axis=0), 16000, "PCM_16")
     """
 
-    vad = VoiceActivityDetection()
     cumulated = []
     precumulated = deque(maxlen=precumulate)
 
-    with MicrophoneStream(**kwargs) as stream:
+    model, _ = torch.hub.load(
+        repo_or_dir="snakers4/silero-vad",
+        model="silero_vad",
+        force_reload=True,
+        trust_repo=True,
+    )
 
-        audio_generator = stream.generator(resample_rate=resample_rate)
-        chunk_length = stream._chunk
-        waveform = np.zeros((max_to_visualize * chunk_length))
-
-        for chunk in audio_generator:
-            # vad.iter(...) should be pass with 'float32'
-            is_speech = vad.iter(chunk[0])
+    with microphone_stream as stream:
+        for chunk in stream:
+            # NOTE: silero-vad configs
+            # chunk_size = 512 if SAMPLING_RATE == 16000 else 256
+            confidence = model(
+                torch.from_numpy(np.copy(chunk[:512, 0])),
+                stream.rate,
+            ).item()
+            is_speech = confidence >= speech_threshold
 
             if is_speech or cumulated:
                 cumulated.append(chunk)
             else:
                 precumulated.append(chunk)
 
-            if (not is_speech and len(cumulated) >= min_to_cumulate) or (
-                len(cumulated) > max_to_cumulate
+            if (
+                not is_speech
+                and len(cumulated) >= min_to_cumulate
+                or len(cumulated) >= max_to_cumulate
             ):
-                waveform = np.concatenate((list(precumulated) + cumulated), axis=-1)
-                yield (stream._rate, waveform * stream._rate)
+                waveform = np.concatenate((list(precumulated) + cumulated), axis=0)
+                yield (stream.rate, waveform)
                 cumulated = []
                 precumulated = deque(maxlen=precumulate)
